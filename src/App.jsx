4b37fef645f58e0b3e6ee7b86ffa72bc03e64278
yg93 <dldusrlf98@gmail.com>
2026-04-20 10:57:14 +0900

    test

--- src/App.jsx
+++ src/App.jsx
@@ -40,7 +40,7 @@ function App() {
       </div>
       <div className='list'>
         <h4>{ 글제목[2] }</h4>
-        <p>2월 17일 발행s</p>
+        <p>2월 17일 발행</p>
       </div>
 
       <Modal></Modal>
