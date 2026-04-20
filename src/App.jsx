3c3c092f69ff9dfc0b6681ee5242904106b285ec
yg93 <dldusrlf98@gmail.com>
2026-04-20 10:10:35 +0900

    업로드

---
+++ src/App.jsx
@@ -0,0 +1,62 @@
+
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동맛집', '파이썬 독학']);
+  let [따봉, 따봉변경] = useState(0);
+
+
+  return (
+    <div className='App'>
+      <div className="black-nav">
+        <h4>블로그임</h4>
+      </div>
+
+      <button onClick={()=>{
+        let 정렬 = [...글제목]
+        정렬.sort();
+        글제목변경(정렬);
+      }}>가나다순정렬</button>
+
+      <button onClick={()=>{
+        let copy = [...글제목];
+        copy[0] = '여자코트 추천';
+        글제목변경(copy);
+      }}>글수정</button>
+
+      <div className='list'>
+        <h4>{ 글제목[0] } <span onClick={ ()=>{ 따봉변경(따봉+1) } }>👍</span> {따봉} </h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className='list'>
+        <h4>{ 글제목[1] }</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className='list'>
+        <h4>{ 글제목[2] }</h4>
+        <p>2월 17일 발행s</p>
+      </div>
+
+      <Modal></Modal>
+
+    </div>
+  )
+}
+
+function Modal(){
+  return (
+    <div className='modal'>
+      <h4>제목</h4>
+      <p>날짜</p>
+      <p>상세내용</p>
+    </div>
+  )
+}
+
+export default App
